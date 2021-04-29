ef6a6ba8dc90cd6785f6114ac51f994742c4b814
CJEF <ef2200@mail.ru>
2021-04-29 11:46:28 +0600

    full

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,6 @@ const swiper = new Swiper(".swiper-container", {
   // Optional parameters
   loop: true,
   spaceBetween: 10,
-  paginationClickable: true,
 
   autoplay: {
     delay: 3000,
@@ -11,6 +10,10 @@ const swiper = new Swiper(".swiper-container", {
   // If we need pagination
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<svg class="swiper-pagination-bullet" width="17" height="20" viewbox="0 0 17 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 14.6163V5.38373L8.5 1.14843L16 5.38373V14.6163L8.5 18.8516L1 14.6163Z" stroke="#FA830B" stroke-width="2"/></svg>';
+    },
   },
 
   // Navigation arrows
@@ -24,3 +27,17 @@ const swiper = new Swiper(".swiper-container", {
     el: ".swiper-scrollbar",
   },
 });
+
+const anchors = document.querySelectorAll('a[href*="#about"]');
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const blockID = anchor.getAttribute("href").substr(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
